2f7f47db889b00d9f34d77dcc523322054b9561f
Milan60 <laczkomilan60@gmail.com>
2025-03-23 17:12:14 +0100

    migráció létrehozása

--- BerAuto/Application/User.py
+++ BerAuto/Application/User.py
@@ -1,6 +1,26 @@
+from Application import db
+#
+from typing import List
+from typing import Optional
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
+from sqlalchemy.types import String
+from werkzeug.security import generate_password_hash, check_password_hash
+class User(db.Model):
+ __tablename__ = "users"
+ id: Mapped[int] = mapped_column(primary_key=True)
+ username: Mapped[str] = mapped_column(String(30))
+ email: Mapped[Optional[str]]
+ password: Mapped[str] = mapped_column(String(30))
+ #posts: Mapped[List["Post"]] = relationship(back_populates="owner")
 
-class User:
-    def __init__(self, username, password, level):
-        self.username = username
-        self.password = password
-        self.level = level
+ def __repr__(self) -> str:
+    return f"User(id={self.id!r}, name={self.name!s}, email={self.email!r})"
+
+ def set_password(self, password):
+    self.password = generate_password_hash(password)
+
+ def check_password(self, password):
+    return check_password_hash(self.password, password)
--- BerAuto/Application/__init__.py
+++ BerAuto/Application/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
 
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db"
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 from Application import routes

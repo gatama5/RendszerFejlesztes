0d76bb8f654c8012133c49fa77cf46737174aee7
bujaaa <bujaki.martin@gmail.com>
2025-03-23 17:44:12 +0100

    Adatbázis migráció

--- BerAuto/Application/__init__.py
+++ BerAuto/Application/__init__.py
@@ -5,12 +5,20 @@ from flask_migrate import Migrate
 
 class Base(DeclarativeBase):
     pass
+
 db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db"
 
 db.init_app(app)
+
 migrate = Migrate(app, db)
+
+from Application.User import User
+from Application.Customer import Customer
+from Application.Owner import Owner
+from Application.CarManager import CarManager
 
 from Application import routes

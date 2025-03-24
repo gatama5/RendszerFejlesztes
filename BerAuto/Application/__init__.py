bdb921bfd53c93bf1ba8b94d3005ab6e3a653790
Csernák László <csernaklacko09@gmail.com>
2025-03-24 22:03:10 +0100

    main push zap fájlból

--- BerAuto/Application/__init__.py
+++ BerAuto/Application/__init__.py
@@ -7,18 +7,15 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
-
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"
+app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"  # Ez fontos a flash üzenetekhez
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db"
-
 db.init_app(app)
-
 migrate = Migrate(app, db)
 
+# Modellek és útvonalak importálása
 from Application.User import User
 from Application.Customer import Customer
 from Application.Owner import Owner
 from Application.CarManager import CarManager
-
 from Application import routes

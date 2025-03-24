83f0315df2c2e0dd06a56b1d73e6c6f613504595
bujaaa <bujaki.martin@gmail.com>
2025-03-24 22:08:28 +0100

    Revert "kért push"
    This reverts commit 6fd331ef964092d4cf48eb776da8f9054149a299.

--- BerAuto/Application/__init__.py
+++ BerAuto/Application/__init__.py
@@ -7,15 +7,18 @@ class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
+
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"  # Ez fontos a flash üzenetekhez
+app.config["SECRET_KEY"] = "qwedasdqweasdqweasd"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydb.db"
+
 db.init_app(app)
+
 migrate = Migrate(app, db)
 
-# Modellek és útvonalak importálása
 from Application.User import User
 from Application.Customer import Customer
 from Application.Owner import Owner
 from Application.CarManager import CarManager
+
 from Application import routes
